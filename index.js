0fd7c8dfecbc184994d75b3db719306aaf9775ca
Seth Ullman <seth.ullman98@gmail.com>
2020-09-03 00:31:17 -0600

    add react-native-navigation boilerplate

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 /**
  * @format
  */
-
-import {AppRegistry} from 'react-native';
+import { Navigation } from 'react-native-navigation';
 import App from './App';
-import {name as appName} from './app.json';
+
+Navigation.registerComponent('com.myApp.WelcomeScreen', () => App);
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'com.myApp.WelcomeScreen'
+            }
+          }
+        ]
+      }
+    }
+  });
+});
 
 AppRegistry.registerComponent(appName, () => App);
