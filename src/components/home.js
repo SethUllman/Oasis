9274301cc797c7cf078b5f89af3043a7abd16410
Seth Ullman <seth.ullman98@gmail.com>
2020-09-10 21:48:09 -0600

    add dynamic button opacity

--- src/components/home.js
+++ src/components/home.js
@@ -17,38 +17,38 @@ const HomeScreen = (props) => {
         colors={["#FE8963", "#D53249"]}>
         <Text style={{flex: 0.1, justifyContent: "center", alignItems: "center", fontSize: 32}}>Players: {players}</Text>
         <View style={{flex: 0.1, width: "90%", flexDirection: "row", justifyContent: "space-between"}}>
-          <TouchableOpacity style={styles.players} onPress={() => {setPlayers(1)}}>
+          <TouchableOpacity style={[styles.players, { backgroundColor: players == 1 ? "rgba(55, 1, 1, 1.0)" : "rgba(55, 1, 1, 0.6)"}]} onPress={() => {setPlayers(1)}}>
             <Text style={styles.text}>1</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.players} onPress={() => {setPlayers(2)}}>
+          <TouchableOpacity style={[styles.players, { backgroundColor: players == 2 ? "rgba(55, 1, 1, 1.0)" : "rgba(55, 1, 1, 0.6)" }]} onPress={() => {setPlayers(2)}}>
             <Text style={styles.text}>2</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.players} onPress={() => {setPlayers(3)}}>
+          <TouchableOpacity style={[styles.players, { backgroundColor: players == 3 ? "rgba(55, 1, 1, 1.0)" : "rgba(55, 1, 1, 0.6)" }]} onPress={() => {setPlayers(3)}}>
             <Text style={styles.text}>3</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.players} onPress={() => {setPlayers(4)}}>
+          <TouchableOpacity style={[styles.players, { backgroundColor: players == 4 ? "rgba(55, 1, 1, 1.0)" : "rgba(55, 1, 1, 0.6)" }]} onPress={() => {setPlayers(4)}}>
             <Text style={styles.text}>4</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.players} onPress={() => {setPlayers(5)}}>
+          <TouchableOpacity style={[styles.players, { backgroundColor: players == 5 ? "rgba(55, 1, 1, 1.0)" : "rgba(55, 1, 1, 0.6)" }]} onPress={() => {setPlayers(5)}}>
             <Text style={styles.text}>5</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.players} onPress={() => { setPlayers(6) }}>
+          <TouchableOpacity style={[styles.players, { backgroundColor: players == 6 ? "rgba(55, 1, 1, 1.0)" : "rgba(55, 1, 1, 0.6)" }]} onPress={() => { setPlayers(6) }}>
             <Text style={styles.text}>6</Text>
           </TouchableOpacity>
         </View>
 
         <Text style={{ flex: 0.1, justifyContent: "center", alignItems: "center", fontSize: 32 }}>Starting Health: {health}</Text>
         <View style={{ flex: 0.1, width: "90%", flexDirection: "row", justifyContent: "space-between" }}>
-          <TouchableOpacity style={styles.health} onPress={() => { setHealth(20) }}>
+          <TouchableOpacity style={[styles.health, { backgroundColor: health == 20 ? "rgba(55, 1, 1, 1.0)" : "rgba(55, 1, 1, 0.6)" }]} onPress={() => { setHealth(20) }}>
             <Text style={styles.text}>20</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.health} onPress={() => { setHealth(30) }}>
+          <TouchableOpacity style={[styles.health, { backgroundColor: health == 30 ? "rgba(55, 1, 1, 1.0)" : "rgba(55, 1, 1, 0.6)" }]} onPress={() => { setHealth(30) }}>
             <Text style={styles.text}>30</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.health} onPress={() => { setHealth(40) }}>
+          <TouchableOpacity style={[styles.health, { backgroundColor: health == 40 ? "rgba(55, 1, 1, 1.0)" : "rgba(55, 1, 1, 0.6)" }]} onPress={() => { setHealth(40) }}>
             <Text style={styles.text}>40</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.health} onPress={() => { setHealth(4) }}>
+          <TouchableOpacity style={[styles.health, { backgroundColor: (health != 20 && health != 30 && health != 40) ? "rgba(55, 1, 1, 1.0)" : "rgba(55, 1, 1, 0.6)" }]} onPress={() => { setHealth(4) }}>
             <Text style={styles.text}>Custom</Text>
           </TouchableOpacity>
       </View>
